bcc7caf287de757702df54e717755005f0207903
MINA ADEL <minaadel837@gmail.com>
2025-01-18 07:49:52 +0200

    first commit

---
+++ Models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    userType: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "userType",
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
