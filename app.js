e1a04bdbf4c90369f3cdc0e972c4ebbe660b35f8
MINA ADEL <minaadel837@gmail.com>
2025-02-11 22:15:26 +0200

    add message

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const port = 3000;
-const express = require('express');
-const connectDB = require('./config/db.config');
-const userRouter = require('./Routers/user.router');
-const userTypeRouter = require('./Routers/userType.router');
-const productRouter = require('./Routers/product.router');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db.config");
+const userRouter = require("./Routers/user.router");
+const userTypeRouter = require("./Routers/userType.router");
+const productRouter = require("./Routers/product.router");
+const cors = require("cors");
 
 const app = express();
-app.use(cors({origin:'http://localhost:4200'}));
+app.use(cors({ origin: "http://localhost:4200" }));
 app.use(express.json());
-app.use('/images',express.static('./imgs'));
+app.use("/images", express.static("./imgs"));
 connectDB();
 
-app.use('/user',userRouter);
-app.use('/usertype',userTypeRouter);
-app.use('/product',productRouter);
+app.use("/user", userRouter);
+app.use("/usertype", userTypeRouter);
+app.use("/product", productRouter);
 
+app.get("/", (req, res) => {
+  res.send("Hello in my MVC Backend API - MINA ADEL");
+});
 
-app.listen(port,()=> console.log(`server started at port ${port}`)
-)
+app.listen(port, () => console.log(`server started at port ${port}`));
