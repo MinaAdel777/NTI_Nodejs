a9c2c16aeb1e364c08c2613d0041b1a20407dc4d
MINA ADEL <minaadel837@gmail.com>
2025-02-25 17:23:45 +0200

    add getproductbyID

--- Controllers/product.controller.js
+++ Controllers/product.controller.js
@@ -1,22 +1,35 @@
-const productModel = require('../Models/product.model');
+const productModel = require("../Models/product.model");
 
-exports.createProduct = async(req,res)=>{
-    try{
-        req.body.imageURL = req.file.filename;
-        const product = await productModel.create(req.body);
-        res.status(201).json(product);
-    }
-    catch(err){
-        res.status(500).json({error:err.message});
-    }
-}
+exports.createProduct = async (req, res) => {
+  try {
+    req.body.imageURL = req.file.filename;
+    const product = await productModel.create(req.body);
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-exports.getProducts= async(req,res)=>{
-    try{
-        const products = await productModel.find();
-        res.status(200).json(products);
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await productModel.find();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-    } catch(err){
-        res.status(500).json({error:err.message});
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await productModel.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
     }
-}
+
+    res.json(product);
+    
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
--- Routers/product.router.js
+++ Routers/product.router.js
@@ -4,6 +4,7 @@ const productController = require("../Controllers/product.controller");
 const upload = require("../config/multerConfig");
 
 router.get("/", productController.getProducts);
+router.get("/:id", productController.getProductById);
 router.post(
   "/",
   upload.single("productImage"),
